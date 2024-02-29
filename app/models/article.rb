9e8945809e0d8c2f604b08251d2b0ec135836860
noramm-12 <a0960186130@gmail.com>
2024-02-29 17:18:07 +0800

    feat:article 新增model驗證

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
-class Article < ApplicationRecord
+# frozen_string_literal: true
 
+class Article < ApplicationRecord
+  validates :title, presence: true,length:{minimum:1,maximun:10}
+  validates :description, presence: true,length:{minimum:1,maximun:100}
 end
