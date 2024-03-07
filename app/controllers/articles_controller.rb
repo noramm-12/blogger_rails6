c3936af9adc068010bde678c50f66a65a1d7739d
noramm-12 <a0960186130@gmail.com>
2024-03-07 16:33:54 +0800

    add new&create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,17 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+
+  end
+
+  def create
+    #render plain: params[:article]
+    #@article = Article.new(params[:article])
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    #render plain: @article.inspect
+    redirect_to article_path(@article)
+  end
 end
